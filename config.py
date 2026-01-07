09b5ce606ece24c8e1a0e45d97b66a60fa58eae4
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2026-01-08 03:47:24 +0530

    Add configuration settings for API and channels

---
+++ config.py
@@ -0,0 +1,16 @@
+API_ID = 2754
+API_HASH = "0b52aa3cb9f"
+SESSION = "premium5_userbot"
+
+ADMIN_CHANNEL = -1003570775504   # yahan message likhega
+LOGGER_CHANNEL = -1003305944384  # report yahan aayegi
+SOURCE_CHANNEL = "@Active_groups_Link"
+
+DELAY = 5               # seconds between messages
+MAX_PER_BATCH = 60      # messages per batch
+BATCH_SLEEP = 4
+
+# Join safety
+JOIN_DELAY = 55           # har join ke beech
+MAX_JOIN_PER_DAY = 55
+
